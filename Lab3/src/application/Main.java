fc8ee0db6707be6c57b88c3910e166deedc336d0
Madeline <1906931+mlizbeth@users.noreply.github.com>
2020-10-12 13:20:35 -0500

    fixes

--- Lab3/src/application/Main.java
+++ Lab3/src/application/Main.java
@@ -5,18 +5,17 @@ import java.io.IOException;
 import application.model.Fleet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		VBox root = new VBox();
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("/Main.fxml"));
-		root = loader.load();
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
+		Parent root = loader.load();
 		
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
--- Lab3/src/application/controller/MainController.java
+++ Lab3/src/application/controller/MainController.java
@@ -1,14 +1,25 @@
 package application.controller;
 
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class MainController implements EventHandler {
+public class MainController implements EventHandler<ActionEvent> {
+	
+	@FXML
+	private Button button;
+	@FXML
+	private TextField textField;
+	@FXML
+	private TextArea textArea;
 
 	@Override
-	public void handle(Event event) {
-		// TODO hook it up!
-		
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("BUtton");
 	}
 
 }
